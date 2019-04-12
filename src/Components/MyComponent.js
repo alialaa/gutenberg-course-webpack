86951c6ab93c01278f76714916491539686e2bef
digital ankit <akjaph@gmail.com>
2019-04-12 13:21:42 +0530

    installed webpack loaders and wordpress browser list config

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class MyComponent extends React.Component {
 	render() {
-		//return <h1>{this.props.title}</h1>
-		return React.createElement('h1', null, `Title: ${this.props.title}`)
+		//return React.createElement('h1', null, `Title: ${this.props.title}`)
+		return <h1>{this.props.title}</h1>
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,19 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
 	}
 }
