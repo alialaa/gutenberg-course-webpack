0dcea0f82ef68e271aa3fb99be3d3d3c60b3fe22
digital ankit <akjaph@gmail.com>
2019-04-12 14:49:22 +0530

    setup react babel preset development mode

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -2,12 +2,15 @@ import React, {Fragment} from 'react';
 
 class MyComponent extends React.Component {
 	render() {
-		return 
-		<Fragment>
-			<h1>{this.props.title}</h1>
-			<p></p>
-		</Fragment>
+		// return 
+		// <Fragment>
+		// 	<h1>{this.props.title}</h1>
+		// 	<p></p>
+		// </Fragment>
 		//return React.createElement('h1', null, `Title: ${this.props.title}`)
+		return <div>
+				{[1,2,3].map(item => <div>{item}</div>)}
+		</div>
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'@babel/preset-env',
-							[
-								'@babel/preset-react',
-								{
-									"pragma": "React.createElement",
-									"pragmaFrag": "React.Fragment",
-									"development"
-								}
+module.exports =  (env, argv) => {
+	var config {
+		entry: './src/index.js',
+		output: {
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+								[
+									'@babel/preset-react',
+									{
+										"pragma": "React.createElement",
+										"pragmaFrag": "React.Fragment",
+										"development": argv.mode === 'development'
+									}
+								]
 							]
-						]
+						}
 					}
 				}
-			}
-		]
-	}
+			]
+		}
+	};
+	return config;
 }
